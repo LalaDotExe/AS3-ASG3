8a7090705bbd8150f9266bbf9f0e1616eed7ed8d
22ftt1499 <22ftt1499@student.pb.edu.bn>
2024-04-15 22:24:54 +0800

    Reconfigure

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,6 @@
-const purgecss = require('@fullhuman/postcss-purgecss')
-
 export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
-    "@fullhuman/postcss-purgecss": {
-      content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-      defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-    },
   },
 }
