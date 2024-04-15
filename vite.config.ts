77ad1abe26333fd1cb03d0333c0598c2da133990
22ftt1499 <22ftt1499@student.pb.edu.bn>
2024-04-16 00:16:36 +0800

    modified base name

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Akmallala/",
+  base: "/AS3-ASG3/",
   plugins: [vue()],
   assetsInclude: ["**/*.JPG"],
 });
