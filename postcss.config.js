06cac4a05947377e7fd45d734bfb25489d7f49ff
22ftt1499 <22ftt1499@student.pb.edu.bn>
2024-04-15 22:19:45 +0800

    Installing purgecss and minified css

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,16 @@
+const purgecss = require('@fullhuman/postcss-purgecss')
+
 export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
+}
+
+module.exports = {
+  plugins: [
+    purgecss({
+      content: ['./**/*.html']
+    })
+  ]
 }
