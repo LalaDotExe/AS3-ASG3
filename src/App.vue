2e6b6499fd04942a17581abcc03702540eeac744
22ftt1499 <22ftt1499@student.pb.edu.bn>
2024-04-15 21:49:18 +0800

    Changing profile pic and description

--- src/App.vue
+++ src/App.vue
@@ -12,9 +12,9 @@ import HelloWorld from "./components/HelloWorld.vue";
 
   <div class="flex flex-col md:flex-row items-center py-4 px-6">
     <img
-      src="https://placehold.co/150"
+      src="./assets/Akmal.JPG"
       alt="Profile Picture"
-      class="w-32 h-32 rounded-full shadow-md mb-4 md:mr-4"
+      class="w-32 h-32 rounded-full object-contain shadow-md mb-4 md:mr-4"
       crossorigin="anonymous"
     />
     <div>
@@ -28,24 +28,25 @@ import HelloWorld from "./components/HelloWorld.vue";
     <h2 class="text-lg font-bold mb-4">About Me</h2>
     <div class="bg-white dark:bg-zinc-300 p-4 rounded-lg shadow-md">
       <p class="text-zinc-600">
-        Assalamualaikum, miss. My name is Mohammad Akmal Shafie'e Bin Roslan,
-        and my course is Diploma in Website Development. My most recent college
-        experience was at IBTE SSRC, where I studied HNtec in Information
-        Technology. Overall my experience in coding has been improving over the
-        years Alhamdulilah. Good communication skills are essential not just in
-        everyday life, but also in the job and in business. Insyallah, during
-        the next ten years, I will gain confidence and financial independence.
+        Hello, My name is Mohammad Akmal Shafie'e Bin Roslan, and my course is
+        Diploma in Website Development. My most recent college experience was at
+        IBTE SSRC, where I studied HNtec in Information Technology. Overall my
+        experience in coding has been improving over the years Alhamdulilah.
+        Good communication skills are essential not just in everyday life, but
+        also in the job and in business. Insyallah, during the next ten years, I
+        will gain confidence and financial independence.
       </p>
     </div>
   </div>
 
-  <div class="mb-8 py-4 px-6">
+  <div class="mb-8 px-6">
     <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
       <div class="bg-white dark:bg-zinc-300 p-4 rounded-lg shadow-md">
         <h3 class="text-lg font-bold mb-2">Hobbies</h3>
         <p class="text-zinc-600">
           Mainly exploring or traveling the world, but I also enjoy hiking and
-          gaming in my leisure time. I also enjoy photography and music.
+          gaming in my leisure time. I also enjoy photography and listening to
+          music.
         </p>
       </div>
       <div class="bg-white dark:bg-zinc-300 p-4 rounded-lg shadow-md">
