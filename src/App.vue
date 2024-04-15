adb7afb1a1547af32e07b54909c78f081ea10228
22ftt1499 <22ftt1499@student.pb.edu.bn>
2024-04-15 20:34:05 +0800

    Adding profile

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,20 @@ import HelloWorld from "./components/HelloWorld.vue";
     <h1>Akmal Shafie</h1>
     <p>Student At Politeknik Brunei</p>
   </header>
+
+  <div class="flex flex-col md:flex-row items-center py-4 px-6">
+    <img
+      src="https://placehold.co/150"
+      alt="Profile Picture"
+      class="w-32 h-32 rounded-full shadow-md mb-4 md:mr-4"
+      crossorigin="anonymous"
+    />
+    <div>
+      <h2 class="text-xl font-bold">John Doe</h2>
+      <p class="text-zinc-600">Computer Science Student</p>
+      <p class="text-zinc-600">University of ABC</p>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
