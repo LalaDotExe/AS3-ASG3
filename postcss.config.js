e107622bbdaedafe479d20e4c164352a31cf2ecb
22ftt1499 <22ftt1499@student.pb.edu.bn>
2024-04-15 22:21:01 +0800

    Updating postcss.config

--- postcss.config.js
+++ postcss.config.js
@@ -4,13 +4,9 @@ export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    "@fullhuman/postcss-purgecss": {
+      content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+      defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+    },
   },
-}
-
-module.exports = {
-  plugins: [
-    purgecss({
-      content: ['./**/*.html']
-    })
-  ]
 }
