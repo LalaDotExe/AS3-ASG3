210a090dcb84eb385e1589c583ab23aa3ee7efb9
22ftt1499 <22ftt1499@student.pb.edu.bn>
2024-04-17 07:50:01 +0800

    latest changes

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
 
   <div class="flex flex-col md:flex-row items-center py-6 px-6">
     <img
-      src="./assets/Akmal.jpeg"
+      src="./assets/Akmal.jpg"
       alt="Profile Picture"
       class="w-32 h-32 rounded-full object-cover shadow-md mb-4 md:mr-4"
       crossorigin="anonymous"
